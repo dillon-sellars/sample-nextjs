b8bcedd809013ea25df889912f0b14c66da67395
Dillon Sellars <dill.sellars@gmail.com>
2024-01-08 20:37:02 -0500

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
-    output: 'export',
-    distDir: "_static",
     images: {
         unoptimized: true
     },
